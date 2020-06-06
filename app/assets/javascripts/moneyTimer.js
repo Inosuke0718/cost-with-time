1cddb41015b34903137c22ffeb9a08a66a846987
Inosuke0718 <63226783+Inosuke0718@users.noreply.github.com>
2020-06-07 00:40:56 +0900

    ImplementFunction2

--- app/assets/javascripts/moneyTimer.js
+++ app/assets/javascripts/moneyTimer.js
@@ -1,26 +1,40 @@
 $(function() {
 
+// Common variants-----------
   let cnt = 2 
+  let calcWageRate = 0;
+  // let calcRsltPast = 0;
+
+// Function1 variants----------
   let timer = document.getElementById('timer');
   let startTime;
   var startTimeChanged = 0;
+  let calcRsltPast = 0;
   var elapsedTime = 0;
   let timerId;
   let timeToadd = 0;
   let calcRslt = 0;
-  let calcWageRate = 0;
-  let calcRsltPast = 0;
   let h;
   let m;
   let s;
 
-// ----------------------------------------------
+// Function2 variants----------
   let timer2 = document.getElementById('cntdwn-timer');
-  var priceAct1 = document.getElementById('cntdwn-cost1');
-  var priceAct2 = document.getElementById('cntdwn-cost2');
-  let remain = 0
-  var maxPrice = 0;
-  let maxPricePast;
+  var startTimeChanged2 = 0;
+  let calcRsltPast2 = 0;
+  var elapsedTime2 = 0;
+  let timerId2;
+  let timeToadd2 = 0;
+  let calcRslt2 = 0;
+  let startTime2;
+  let h2;
+  let m2;
+  let s2;
+
+    let priceAct2 = document.getElementById('cntdwn-cost2');
+    let remain = 0
+    let resetSignal = 1;
+
 
 // 共通機能-------------------------------------------
   $('#add').click(function(){
@@ -77,7 +91,6 @@ $(function() {
 
 // ここからFunction1の機能--------------
   function calcPrice(){
-    // debugger
     calcRslt = calcWageRate * ((elapsedTime - startTimeChanged) / 1000)+ calcRsltPast
     price__act.textContent = Math.round(calcRslt)
   }
@@ -125,7 +138,7 @@ $(function() {
     calcWageRate = 0;
     calcRslt = 0;
     calcRsltPast = 0;
-    startTimeChange =0;
+    startTimeChanged =0;
     price__act.textContent = "0"
 
     updateTimetText1()
@@ -137,7 +150,6 @@ $(function() {
   });
 
   $(document).on("click", "#save",function(){
-
     var recTime = elapsedTime / 1000
     var dataTimer = {'timer' :{ "money" :Math.round(calcRslt), "time" :recTime, "wage" :calcWageRate * 3600 / cntPeople , "people" :cntPeople}};
 
@@ -177,57 +189,58 @@ $(function() {
 // ここからFunction２の機能--------------
 
 $('#cntdwn-play').click(function(){
-  let h = $(`#cntdwn-h`).val();
-  let m = $(`#cntdwn-m`).val();
-  let s = $(`#cntdwn-s`).val();
+  h2 = $(`#cntdwn-h`).val();
+  m2 = $(`#cntdwn-m`).val();
+  s2 = $(`#cntdwn-s`).val();
 
-  if(String(h).length == 0 && String(m).length == 0 && String(s).length == 0){
+  if(String(h2).length == 0 && String(m2).length == 0 && String(s2).length == 0){
     alert('秒数を入力してください');
   } else{
 
-    if(String(h).length == 1){
-      h = '0' + h 
-    } else if(String(h).length == 0){
-      h = '00' 
+    if(String(h2).length == 1){
+      h2 = '0' + h2 
+    } else if(String(h2).length == 0){
+      h2 = '00' 
     }
-    if(String(m).length == 1){
-      m = '0' + m 
-    }else if(String(m).length == 0){
-      m = '00' 
+    if(String(m2).length == 1){
+      m2 = '0' + m2 
+    }else if(String(m2).length == 0){
+      m2 = '00' 
     }
-    if(String(s).length == 1){
-      s = '0' + s
-    }else if(String(s).length == 0){
-      s = '00' 
+    if(String(s2).length == 1){
+      s2 = '0' + s2
+    }else if(String(s2).length == 0){
+      s2 = '00' 
     }
-    setTime = Number(h) * 3600 + Number(m) * 60 + Number(s)
-    startTime = Date.now();
+    setTime2 = Number(h2) * 3600 + Number(m2) * 60 + Number(s2)
+    startTime2 = Date.now();
     cntdwntime()
   }
 });
 
   function cntdwntime(){
-    timerId = setTimeout(function(){
-      elapsedTime = (Date.now() - startTime + timeToadd) / 1000 - 1;
-      remain = setTime - elapsedTime;
+    timerId2 = setTimeout(function(){
+      elapsedTime2 = (Date.now() - startTime2 + timeToadd2) / 1000 - 1
+      // debugger
+      remain = setTime2 - elapsedTime2;
       cntdwntime();
       updateTimetText2()
     },100);
   }
 
   function updateTimetText2(){
-    h = Math.floor(remain / 3600);
-    h = ('0' + h).slice(-2); 
-    m = Math.floor(remain / 60) - h * 60
-    m = ('0' + m).slice(-2); 
-    s = Math.floor(remain % 60);
-    s = ('0' + s).slice(-2);
+    h2 = Math.floor(remain / 3600);
+    h2 = ('0' + h2).slice(-2); 
+    m2 = Math.floor(remain / 60) - h2 * 60
+    m2 = ('0' + m2).slice(-2); 
+    s2 = Math.floor(remain % 60);
+    s2 = ('0' + s2).slice(-2);
 
-    if(h == 00 && m == 00 && s == 00){
+    if(h2 == 00 && m2 == 00 && s2 == 00){
       popup()
       reset()
     }
-    timer2.textContent = h + ':' +  m + ':' + s
+    timer2.textContent = h2 + ':' +  m2 + ':' + s2
   }
 
   $('#cntdwn-play').click(function(){
@@ -236,8 +249,8 @@ $('#cntdwn-play').click(function(){
     wageMultiPeople() 
   });
   $('#cntdwn-stop').click(function(){
-    clearTimeout(timerId);
-    timeToadd += Date.now() - startTime;
+    clearTimeout(timerId2);
+    timeToadd2 += Date.now() - startTime2;
     document.getElementById("cntdwn-stop").style.display ="none";
     document.getElementById("cntdwn-play").style.display ="block";
   });
@@ -245,10 +258,11 @@ $('#cntdwn-play').click(function(){
     reset()
   });
   function reset(){
-    clearTimeout(timerId);
+    clearTimeout(timerId2);
     timer2.textContent = '00:00:00'
-    timeToadd = 0
-    calcWageRate = 0
+    timeToadd2 = 0
+    resetSignal = 0
+    startTimeChanged2 =0;
     if($('#cntdwn-stop').css('display') == 'block'){
       document.getElementById("cntdwn-stop").style.display ="none";
       document.getElementById("cntdwn-play").style.display ="block";  
@@ -256,11 +270,10 @@ $('#cntdwn-play').click(function(){
   }
 
   var observer = new MutationObserver(function(){
-
     $("#reg-info").change(function() {
-      startTimeChanged = elapsedTime;
-      calcRsltPast = 0;
-      calcRsltPast =  calcRslt;
+      startTimeChanged2 = elapsedTime2;
+      calcRsltPast2 = 0;
+      calcRsltPast2 =  calcRslt2;
       wageMultiPeople()
     });
     calcPrice2()
@@ -273,11 +286,46 @@ $('#cntdwn-play').click(function(){
   observer.observe(elem2, config2);
 
   function calcPrice2(){
-    calcRslt = calcWageRate * ((elapsedTime + 1 - startTimeChanged) )+ calcRsltPast
-    priceAct2.textContent = Math.round(calcRslt)
-    // debugger
+    calcRslt2 = (calcWageRate * ((elapsedTime2 + 1 - startTimeChanged2) )+ calcRsltPast2) * resetSignal
+    priceAct2.textContent = Math.round(calcRslt2)
+    resetSignal = 1
   }
 
+  
+  $(document).on("click", "#cntdwn-save",function(){
+    var recTime = elapsedTime2 / 1000
+    var dataTimer2 = {'timer' :{ "money" :Math.round(calcRslt), "time" :recTime, "wage" :calcWageRate * 3600 / cntPeople , "people" :cntPeople}};
+
+    $.ajaxPrefilter(function(options, originalOptions, jqXHR) {
+      var token;
+      if (!options.crossDomain) {
+      token = $('meta[name="csrf-token"]').attr('content');
+        if (token) {
+            return jqXHR.setRequestHeader('X-CSRF-Token', token);
+          }
+      }
+    });
+
+    $.ajax({
+      url: "/timer",
+      type: "POST",
+      data: dataTimer2,
+      dataType: 'json',
+    })
+    .done(function(){
+      alert('saved');
+    })
+    .fail(function(){
+      var result = window.confirm("please login or signin");
+      if( result ) {
+        console.log("ログイン画面に移動します")
+        window.location.href = "/users/sign_in"
+      }
+      else {
+        console.log("キャンセルされました。")
+      }
+    })
+  });
 
 //タイマーストップ告知PopUp ----------------------------------------
   function popup(){
@@ -298,9 +346,51 @@ $('#cntdwn-play').click(function(){
       })
     }
   }
-}); 
 
+  $(document).on("click", "#cntdwn-save2",function(){
+    var popup = document.getElementById('js-popup');
+    var recTime = elapsedTime2 / 1000
+    var dataTimer3 = {'timer' :{ "money" :Math.round(calcRslt), "time" :recTime, "wage" :calcWageRate * 3600 / cntPeople , "people" :cntPeople}};
+
+    $.ajaxPrefilter(function(options, originalOptions, jqXHR) {
+      var token;
+      if (!options.crossDomain) {
+      token = $('meta[name="csrf-token"]').attr('content');
+        if (token) {
+            return jqXHR.setRequestHeader('X-CSRF-Token', token);
+          }
+      }
+    });
+
+    $.ajax({
+      url: "/timer",
+      type: "POST",
+      data: dataTimer3,
+      dataType: 'json',
+    })
+    .done(function(){
+      alert('saved');
+      popup.classList.remove('is-show');
+    })
+    .fail(function(){
+      var result = window.confirm("please login or signin");
+      if( result ) {
+        console.log("ログイン画面に移動します")
+        window.location.href = "/users/sign_in"
+      }
+      else {
+        console.log("キャンセルされました。")
+      }
+    })
+  });
+
+}); 
 // 追加実装---------------------------
+// var priceAct1 = document.getElementById('cntdwn-cost1');
+
+// var maxPrice = 0;
+// let maxPricePast;
+
   // ここでMax値を計算している
   // function calcMaxPrice(){
   //   maxPrice = maxPricePast + (calcWageRate * (setTime - elapsedTime))
